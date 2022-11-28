3345712611b6120d456847c70d530d324c9bc1c4
Darshan Santosh Borse <90395345+DarshanBorse@users.noreply.github.com>
2022-11-28 10:34:28 +0000

    Home route created.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hellow to codespaaces");
+const app = require("./app");
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
